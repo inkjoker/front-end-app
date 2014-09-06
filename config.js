6b6eed5597403d2d2608935bc4ea7611bcdabee2
@solyanik <sergey.solyanik@cactussoft.biz>
2014-09-06 15:38:45 +0300

    r

--- config.js
+++ config.js
@@ -2,26 +2,14 @@ require.config({
 	baseUrl : (function() {
             var baseUrl = window.location.href.split('/');
             //url = baseUrl[0] + '//' +baseUrl[2] + '/djs/';
-            url = "file:///C:/WebServers/home/knife.loc/html/js"
+            url = ""
             return url;
 	})(),
 	'paths' : {
 		'jquery' : 'vendor/jquery',
-		'validate' : 'vendor/jquery.validate',
-		'message' : 'vendor/messages_ru',
-		'customSelect' : 'vendor/jquery.customSelect',
-		'perfect-scrollbar' : 'vendor/perfect-scrollbar',
-		'bxslider' : 'vendor/bxSlider/jquery.bxslider',
-		'popup' : 'vendor/jquery.magnific-popup',
 		'jsrender' : 'vendor/jsrender'
 	},
 	'shim' : {
-		'validate' : ['jquery'],
-		'message' : ['validate'],
-		'bxslider' : ['jquery'],
-		'perfect-scrollbar' : ['jquery'],
-		'popup' : ['jquery'],
-		'customSelect' : ['jquery'],
 		'jsrender' : ['jquery']
 	},
 	urlArgs: "bust=" +  (new Date()).getTime()
@@ -29,6 +17,6 @@ require.config({
 
 var dimenArray = dimenArray || [];
 
-require(['main'], function(){
+require(['app'], function(){
 	require(dimenArray);
 })
